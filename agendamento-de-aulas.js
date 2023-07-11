2257774ed2c7802e74d6c3c794d3d65f57401b3f
CFSJCODE <claudiofranciscojunior2006@gmail.com>
2023-07-10 22:26:53 -0300

    Adicionado o formulario de seleção de datas

---
+++ agendamento-de-aulas.js
@@ -0,0 +1,15 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    var data = document.getElementById('data').value;
+    var horario = document.getElementById('horario').value;
+
+    var startDateTime = new Date(data + 'T' + horario + ':00');
+    var endDateTime = new Date(startDateTime.getTime() + 45 * 60 * 1000); // 45 minutos depois
+
+    console.log('Data e horário selecionados:');
+    console.log('Início:', startDateTime);
+    console.log('Fim:', endDateTime);
+    
+    // Aqui você pode adicionar o código para agendar a aula, como fazer chamadas de API para o Google Agenda, por exemplo.
+  });
