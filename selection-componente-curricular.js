16c1c1121ebfdf0919ef314ac23457e80d4876b3
CFSJCODE <claudiofranciscojunior2006@gmail.com>
2023-07-15 22:12:57 -0300

    Reformulação e refatoração de código

--- selection-componente-curricular.js
+++ selection-componente-curricular.js
@@ -1,38 +1,28 @@
 function handleCampoDeEstudoChange() {
     var campoDeEstudoSelect = document.getElementById("Seleção-De-Campo-De-Estudo");
     var componenteCurricularSelect = document.getElementById("Seleção-De-Componente-Curricular");
+    var options = componenteCurricularSelect.options;
 
-    if (campoDeEstudoSelect.value === "Ciências Exatas") {
-        componenteCurricularSelect.options[0].selected = true; // Defina a primeira opção como selecionada por padrão
-        componenteCurricularSelect.options[0].disabled = true; // Desative a primeira opção
+    componenteCurricularSelect.options[0].selected = true; // Defina a primeira opção como selecionada por padrão
+    componenteCurricularSelect.options[0].disabled = true; // Desative a primeira opção
 
-        var options = componenteCurricularSelect.options;
-        for (var i = 1; i < options.length; i++) {
-            var option = options[i];
-            var value = option.value;
+    for (var i = 1; i < options.length; i++) {
+        var option = options[i];
+        var value = option.value;
+
+        if (campoDeEstudoSelect.value === "Ciências Exatas") {
             if (value === "Matemática" || value === "Estratégias Financeiras" || value === "Matemática Aplicada A Computação") {
                 option.disabled = false; // Habilita as opções desejadas
             } else {
                 option.disabled = true; // Desabilita as opções indesejadas
             }
-        }
-    } else if (campoDeEstudoSelect.value === "Ciências Da Natureza") {
-        componenteCurricularSelect.options[0].selected = true; // Defina a primeira opção como selecionada por padrão
-        componenteCurricularSelect.options[0].disabled = true; // Desative a primeira opção
-
-        var options = componenteCurricularSelect.options;
-        for (var i = 1; i < options.length; i++) {
-            var option = options[i];
-            var value = option.value;
+        } else if (campoDeEstudoSelect.value === "Ciências Da Natureza") {
             if (value === "Biotecnologia" || value === "Astrofísica" || value === "Física Teorica") {
                 option.disabled = false; // Habilita as opções desejadas
             } else {
                 option.disabled = true; // Desabilita as opções indesejadas
             }
-        }
-    } else {
-        for (var i = 0; i < componenteCurricularSelect.options.length; i++) {
-            var option = componenteCurricularSelect.options[i];
+        } else {
             option.disabled = false; // Habilita todas as opções
         }
     }
